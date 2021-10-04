e39bcafdc4179a1bea78fa219dce62637dc519b8
ChabenatQuentin <qchabenat.work@gmail.com>
2021-10-04 16:28:17 +0200

    localUserData.ps1

--- localAccountData.ps1
+++ localAccountData.ps1
@@ -1,1 +1,6 @@
-﻿Write-Host "test"
+﻿Write-Host "Local account data : `n"
+foreach($user in Get-LocalUser){
+    $user | Select-Object Name,FullName,SID,LastLogon,PasswordExpire,PasswordLastSet,PasswordChangeableDate,AccountExpires,Enabled | Format-List
+    Write-Host "File permissions :`n"
+    .\accesschk.exe $user c:\* -s
+}
